dc9752a38d66e26657340d9df59eb91ef8543962
Darshan-693 <136052457+Darshan-693@users.noreply.github.com>
2024-11-11 21:36:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors());
+app.use(cors(
+    {
+        origin: "https://magnificent-custard-8347e1.netlify.app"
+    }
+));
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/',require('./routes'));
